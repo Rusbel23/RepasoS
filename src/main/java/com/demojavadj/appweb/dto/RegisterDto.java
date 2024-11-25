9f941965ce0aff34549ee16b2d946937237c4424
Rusbel23 <juanrusbelusca@gmail.com>
2024-11-25 12:45:22 -0500

    dto de user

--- src/main/java/com/demojavadj/appweb/dto/RegisterDto.java
+++ src/main/java/com/demojavadj/appweb/dto/RegisterDto.java
@@ -1,4 +1,90 @@
 package com.demojavadj.appweb.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import org.aspectj.bridge.IMessage;
+
 public class RegisterDto {
+    @NotEmpty
+    private String firstName;
+    @NotEmpty
+    private String lastName;
+    @NotEmpty
+    @Email
+    private String email;
+    private String dni;
+    private String direction;
+    @Size(min =6,message = "Minimum Password length id")
+    private String password;
+    private String confirmPassword;
+
+    public RegisterDto() {
+    }
+
+    public RegisterDto(String firstName, String lastName, String email, String dni, String direction, String password, String confirmPassword) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.dni = dni;
+        this.direction = direction;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
 }
--- src/main/java/com/demojavadj/appweb/services/UsersService.java
+++ src/main/java/com/demojavadj/appweb/services/UsersService.java
@@ -1,12 +1,14 @@
 package com.demojavadj.appweb.services;
 
+import com.demojavadj.appweb.dto.RegisterDto;
 import com.demojavadj.appweb.models.Users;
 import java.util.List;
 
 public interface UsersService {
-    List<Users> getAllUsers();
+    List<Users> allListUsers();
     void addUser(Users user, String roleName);
     Users getUserById(Long id);
     void deleteUser(Long id);
     void updateUser(Users user);
+    Users saveUser(RegisterDto registerDto);
 }
